0b9ef2a2271267b82e3b56ce2a38afa27fc76a6f
New Dad <josh_panakkal3122@hotmail.com>
2020-07-07 22:06:06 -0400

    render search history to page

--- script.js
+++ script.js
@@ -1,90 +1,142 @@
 // API key
 var APIKey = "645a6f915fa06385ae3c8b3689dadec1";
 
-renderWeather();
-renderForecast();
+var cityArray = [];
+var weather;
+var uvIndex;
+var forecast;
+
+init();
+
+// // localStorage.setItem("Array", JSON.stringify(array));
+
+// // renderWeather();
+// // renderForecast();
+
 
 // Event listener
 $("button").on("click", function(event){
-  event.preventDefault();
+  event.preventDefault(); 
+  
+// Store search value into variable
+  var cityName = $("#input").val().trim(); 
+  console.log(cityName);
+  
 
-  // Store search value into variable
-  var cityName = $("#input").val().trim();    
+//     // var y = JSON.parse(localStorage.getItem("Array") || []);
+  cityArray.push(cityName);
+  localStorage.setItem("cityArray", JSON.stringify(cityArray));
+//     // localStorage.setItem("Array", JSON.stringify(y));
+    
+//     // $("#list1").text(y[0]);
+//     // $("#list2").text(y[1]);
+//     // $("#list3").text(y[2]);
+//     // $("#list4").text(y[3]);
+//     // $("#list5").text(y[4]);
+//     // $("#list6").text(y[5]);
+//     // $("#list7").text(y[6]);
+//     // $("#list8").text(y[7]);
 
-  // Weather API url including search input value and API key
+     
+
+  //Weather API url including search input value and API key
   var weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&cnt={5}&appid=" + APIKey;  
     
-  // AJAX call to the OpenWeatherMap API for CURRENT WEATHER
+  //AJAX call to the OpenWeatherMap API for CURRENT WEATHER
   $.ajax({
   url: weatherURL,
   method: "GET"}).then(function(response) {
-    localStorage.setItem("weather", JSON.stringify(response));
-    renderWeather();
-    renderForecast();
+  weather = response;
+  localStorage.setItem("weather", JSON.stringify(weather));
+  console.log(weather);
+  
+  
     
-    // Convert longitude and latitude values into strings and store in variables
-    var longitude = JSON.stringify(response.coord.lon);
-    var latitude = JSON.stringify(response.coord.lat);
+    
+  //Convert longitude and latitude values into strings and store in variables
+  var longitude = JSON.stringify(response.coord.lon);
+  var latitude = JSON.stringify(response.coord.lat);
 
-    // Weather API url for UV Index including API key and longitude/latitude coordinates
+  //Weather API url for UV Index including API key and longitude/latitude coordinates
     var uvURL = "http://api.openweathermap.org/data/2.5/uvi?appid=" + APIKey + "&lat=" + latitude + "&lon=" + longitude;
 
-    // AJAX call to the OpenWeatherMap API for CURRENT UV Index
+  // AJAX call to the OpenWeatherMap API for CURRENT UV Index
     $.ajax({
     url: uvURL,
     method: "GET"}).then(function(response) {
-    localStorage.setItem("uvIndex", JSON.stringify(response)); 
-    renderWeather();     
-    renderForecast(); 
-    }) 
+    uvIndex = response; 
+    localStorage.setItem("uvIndex", JSON.stringify(uvIndex));
+    console.log(uvIndex);
+    
 
-    // Weather API url for 5 day forecast including API key and longitude/latitude coordinates
-    var forecastURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude={minutely}&appid=" + APIKey;    
-          
-    //AJAX call to the OpenWeatherMap API for 5 day forecast
-    $.ajax({
-    url: forecastURL,
-    method: "GET"
-    }).then(function(response){
-      console.log(response)        ;
-      localStorage.setItem("forecast", JSON.stringify(response));  
-      renderWeather(); 
-      renderForecast();       
-    })
+      // Weather API url for 5 day forecast including API key and longitude/latitude coordinates
+        var forecastURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude={minutely}&appid=" + APIKey;    
+              
+        //AJAX call to the OpenWeatherMap API for 5 day forecast
+        $.ajax({
+        url: forecastURL,
+        method: "GET"
+        }).then(function(response){
+                  ;
+        forecast = response;
+        localStorage.setItem("forecast", JSON.stringify(forecast));  
+        console.log(forecast);
+        renderWeather(); 
+        renderForecast(); 
+                
+      })    
+    })    
   })   
 })  
 
-function renderWeather(){
-  var city = JSON.parse(localStorage.getItem("weather"));
-  var uvIndex = JSON.parse(localStorage.getItem("uvIndex"));
-  var forecast = JSON.parse(localStorage.getItem("forecast"));
+function init(){
+  weatherInit = JSON.parse(localStorage.getItem("weather"));
+  uvIndexInit = JSON.parse(localStorage.getItem("uvIndex"));
+  forecastInit = JSON.parse(localStorage.getItem("forecast"));
+  cityArrayInit = JSON.parse(localStorage.getItem("cityArray"));
+
+  if(weatherInit && uvIndexInit && forecastInit && cityArrayInit){
+    weather = weatherInit;
+    uvIndex = uvIndexInit;
+    forecast = forecastInit;
+    cityArray = cityArrayInit;
+
+    renderWeather();
+    renderForecast();
+    renderHistory();
+  }
+  else{
+    return;
+  }         
+ }
+
+function renderWeather(){  
   
   // Convert the temp to fahrenheit
-  var tempF = (city.main.temp - 273.15) * 1.80 + 32;
+  var tempF = (weather.main.temp - 273.15) * 1.80 + 32;
   
   //Convert timestamp to readable format
-  var m = moment.unix(city.dt).format("M-DD-YYYY");      
+  var m = moment.unix(weather.dt).format("M-DD-YYYY");      
   $(".date").text("(" + m.toString(2) + ")"); 
     
   // Transfer content to HTML
-  $(".city").text(city.name);
+  $(".city").text(weather.name);
   $(".tempF").text("Temperature: " + tempF.toFixed(2));
-  $(".humidity").text("Humidity: " + city.main.humidity);
-  $(".wind").text("Wind Speed: " + city.wind.speed); 
+  $(".humidity").text("Humidity: " + weather.main.humidity);
+  $(".wind").text("Wind Speed: " + weather.wind.speed); 
   $(".uvIndex").text(uvIndex.value);   
 
   //Weather Icon
-  var iconCode = city.weather[0].icon;
+  var iconCode = weather.weather[0].icon;
   var iconURL = "http://openweathermap.org/img/w/" + iconCode + ".png";
   $("#wicon").attr("src", iconURL); 
+
+  renderHistory();
 }
 
+//5 Day forecast 
 function renderForecast(){
-  var city = JSON.parse(localStorage.getItem("weather"));
-  var uvIndex = JSON.parse(localStorage.getItem("uvIndex"));
-  var forecast = JSON.parse(localStorage.getItem("forecast"));
-
-  //5 day forecast Day 1
+  
   //Timestamp
   var m = moment.unix(forecast.daily[0].dt).format("MM-DD-YYYY"); 
   $("#title1").text(m.toString());
@@ -138,6 +190,30 @@ function renderForecast(){
   var hum5 = forecast.daily[4].humidity;
   $("#humDay5").text("Humidity: " + hum5);
 }
+
+function renderHistory(){
+  
+  $("#list1").text(cityArray[0]);
+  $("#list2").text(cityArray[1]);
+  $("#list3").text(cityArray[2]);
+  $("#list4").text(cityArray[3]);
+  $("#list5").text(cityArray[4]);
+  $("#list6").text(cityArray[5]);
+  $("#list7").text(cityArray[6]);
+  $("#list8").text(cityArray[7]);
+}
+
+
+
+
+
+
+
+  
+
+
+ 
+
   
   
   
